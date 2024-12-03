8bac6d61e80e9e723039347614603f831b2e8d23
mhepburn <mhepburn@podfather.com>
2024-12-03 15:47:40 +0000

    Solve 03/B in Python

--- 03 - Mull It Over/python/solve_a.py
+++ 03 - Mull It Over/python/solve_a.py
@@ -1,3 +1,4 @@
+from common import evaluate_mul
 from loader import input_as_string
 import re
 
@@ -6,11 +7,6 @@ if __name__ == "__main__":
 
     matches = re.findall('mul\\([0-9]+,[0-9]+\\)', input)
 
-    total = 0
-    for match in matches:
-        factor_strings = re.findall('[0-9]+', match)
-        if len(factor_strings) != 2:
-            raise Exception("Unexpected length for factor string in " + match)
-        total += int(factor_strings[0]) * int(factor_strings[1])
+    total = sum([evaluate_mul(mul) for mul in matches])
 
     print(total)
---
+++ 03 - Mull It Over/python/solve_b.py
@@ -0,0 +1,23 @@
+from common import evaluate_mul
+from loader import input_as_string
+import re
+
+if __name__ == "__main__":
+    input = input_as_string(__file__)
+
+    matches = re.findall('mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don\'t\\(\\)', input)
+
+    total = 0
+    doing = True
+    for match in matches:
+        if match == 'do()':
+            doing = True
+            continue
+        elif match == 'don\'t()':
+            doing = False
+            continue
+
+        if doing:
+            total += evaluate_mul(match)
+
+    print(total)
