485055ca138e5a51cbfea5dbf3220486c1d529fc
mhepburn <mhepburn@podfather.com>
2024-12-04 20:47:02 +0000

    Solve 04/B in Python

--- 04 - Ceres Search/python/solve_b.py
+++ 04 - Ceres Search/python/solve_b.py
@@ -1,14 +1,15 @@
 from loader import input_as_strings
 
 def mark(input, board, points):
-    for [i, j] in points:
-        board[i][j] = input[i][j]
+    for [a, b] in points:
+        if input[a][b] == "A":
+            board[a][b] = board[a][b] + 1
     return board
 
 if __name__ == "__main__":
     input = input_as_strings()
 
-    target = "XMAS"
+    target = "MAS"
     max_i = len(input) - 1
     max_j = len(input[0]) - 1
 
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     for i in range(0, max_i + 1):
         line = []
         for j in range(0, max_j):
-            line.append(".")
+            line.append(0)
         points.append(line)
 
     start_points = []
@@ -25,47 +26,13 @@ if __name__ == "__main__":
             if input[i][j] == target[0]:
                 start_points.append([i, j])
 
-    matches = 0
     for [i,j] in start_points:
-        if j + len(target) - 1 <= max_j:
-            i_s = [ i for _ in range(0, len(target))]
-            j_s = range(j, j + len(target))
-            forward = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
-            assert len(forward) == len(target)
-            if forward == target:
-                matches = matches + 1
-                points = mark(input, points, zip(i_s, j_s))
-        if j - len(target) + 1 >= 0:
-            i_s = [ i for _ in range(0, len(target))]
-            j_s = range(j, j - len(target), -1)
-            backward = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
-            assert len(backward) == len(target)
-            if backward == target:
-                matches = matches + 1
-                points = mark(input, points, zip(i_s, j_s))
-        if i + len(target) - 1 <= max_i:
-            i_s = range(i, i + len(target))
-            j_s = [ j for _ in range(0, len(target))]
-            down = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
-            assert len(down) == len(target)
-            if down == target:
-                matches = matches + 1
-                points = mark(input, points, zip(i_s, j_s))
-        if i - len(target) + 1 >= 0:
-            i_s = range(i, i - len(target), -1)
-            j_s = [j for _ in range(0, len(target))]
-            up = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
-            assert len(up) == len(target)
-            if up == target:
-                matches = matches + 1
-                points = mark(input, points, zip(i_s, j_s))
         if i + len(target) - 1 <= max_i and j + len(target) - 1 <= max_j:
             i_s = range(i, i + len(target))
             j_s = range(j, j + len(target))
             down_right = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
             assert len(down_right) == len(target)
             if down_right == target:
-                matches = matches + 1
                 points = mark(input, points, zip(i_s, j_s))
         if i + len(target) - 1 <= max_i and j - len(target) + 1 >= 0:
             i_s = range(i, i + len(target))
@@ -73,7 +40,6 @@ if __name__ == "__main__":
             down_left = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
             assert len(down_left) == len(target)
             if down_left == target:
-                matches = matches + 1
                 points = mark(input, points, zip(i_s, j_s))
         if i - len(target) + 1 >= 0 and j + len(target) - 1 <= max_j:
             i_s = range(i, i - len(target), -1)
@@ -81,7 +47,6 @@ if __name__ == "__main__":
             up_right = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
             assert len(up_right) == len(target)
             if up_right == target:
-                matches = matches + 1
                 points = mark(input, points, zip(i_s, j_s))
         if i - len(target) + 1 >= 0 and j - len(target) + 1 >= 0:
             i_s = range(i, i - len(target), -1)
@@ -89,10 +54,15 @@ if __name__ == "__main__":
             up_left = "".join(input[x][y] for [x,y] in zip(i_s, j_s))
             assert len(up_left) == len(target)
             if up_left == target:
-                matches = matches + 1
                 points = mark(input, points, zip(i_s, j_s))
 
     # for line in points:
-    #     print("".join(line))
+    #     print("".join([str(x) for x in line]))
 
-    print(matches)
+    xmas_count = 0
+    for line in points:
+        for elem in line:
+            if elem > 1:
+                xmas_count += 1
+
+    print(xmas_count)
