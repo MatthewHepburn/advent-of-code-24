2f236f32c94d77facd0e983d47e7f336601cca2a
mhepburn <mhepburn@podfather.com>
2024-12-06 15:23:15 +0000

    Refactor

---
+++ 06 - Guard Gallivant/python/common.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+
+from grid import Direction, GridPoint, is_in_bounds
+
+
+def get_visted_if_terminates(board: List[List[str]], start: GridPoint) -> Optional[List[List[List[Direction]]]]:
+    guard_position = start
+    direction: Direction = Direction.UP
+
+    # Record every direction we've gone in for each point, to allow detecting loops
+    visited: List[List[List[Direction]]] = []
+    for i in range(0, len(board)):
+        line = []
+        for j in range(0, len(board[i])):
+            line.append([])
+        visited.append(line)
+
+    visited[guard_position.i][guard_position.j].append(direction)
+
+    in_bounds = True
+    while in_bounds:
+        next_pos = guard_position.move(direction.value)
+        next_pos_value = None
+        next_pos_inbounds = is_in_bounds(board, next_pos)
+        if next_pos_inbounds:
+            next_pos_value = board[next_pos.i][next_pos.j]
+        if next_pos_value == '#':
+            # Unable to go this way, rotate:
+            direction = direction.rotate_clockwise()
+            continue
+
+        guard_position = next_pos
+        in_bounds = next_pos_inbounds
+        if in_bounds:
+            if direction in visited[guard_position.i][guard_position.j]:
+                # In a loop!
+                return None
+            visited[guard_position.i][guard_position.j].append(direction)
+
+    return visited
--- 06 - Guard Gallivant/python/solve_a.py
+++ 06 - Guard Gallivant/python/solve_a.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 
+from common import get_visted_if_terminates
 from grid import GridPoint, Direction, is_in_bounds
 from loader import input_as_chars
 
@@ -13,41 +14,16 @@ def position_of(grid: List[List[str]], needles: List[str]) -> Optional[GridPoint
 if __name__ == "__main__":
     board = input_as_chars()
 
-    visited: List[List[bool]] = []
-    for i in range(0, len(board)):
-        line = []
-        for j in range(0, len(board[i])):
-            line.append(False)
-        visited.append(line)
-
     guard_position = position_of(board, ['^'])
     if guard_position is None:
         raise Exception("No start pos")
 
-    visited[guard_position.i][guard_position.j] = True
-    direction: Direction = Direction.UP
+    visited = get_visted_if_terminates(board, guard_position)
 
-    in_bounds = True
-    while in_bounds:
-        next_pos = guard_position.move(direction.value)
-        next_pos_value = None
-        next_pos_inbounds = is_in_bounds(board, next_pos)
-        if next_pos_inbounds:
-            next_pos_value = board[next_pos.i][next_pos.j]
-        if  next_pos_value == '#':
-            # Unable to go this way, rotate:
-            direction = direction.rotate_clockwise()
-            continue
+    visited_total = 0
+    for i in range(0, len(visited)):
+        for j in range(0, len(visited[i])):
+            if len(visited[i][j]) > 0:
+                visited_total = visited_total + 1
 
-        guard_position = next_pos
-        in_bounds = next_pos_inbounds
-        if in_bounds:
-            visited[guard_position.i][guard_position.j] = True
-
-    visted_total = 0
-    for i in range(0, len(board)):
-        for j in range(0, len(board[i])):
-            if visited[i][j]:
-                visted_total = visted_total + 1
-
-    print(visted_total)
+    print(visited_total)
